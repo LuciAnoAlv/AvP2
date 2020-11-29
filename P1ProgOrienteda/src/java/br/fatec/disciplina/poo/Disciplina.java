205712764f62401b03d5044721dcbe9234cab0cc
LuciAnoAlv <luciano.alves96@gmail.com>
2020-11-28 23:08:01 -0300

    Alteração da classe Disciplina
    Alteração ok

--- P1ProgOrienteda/src/java/br/fatec/disciplina/poo/Disciplina.java
+++ P1ProgOrienteda/src/java/br/fatec/disciplina/poo/Disciplina.java
@@ -6,22 +6,29 @@
 
 
 package br.fatec.disciplina.poo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.*;
+import web.DbListener;
 /**
  *
  * @author Luciano
  */
 public class Disciplina {
 
-    
+    private static final String CLASS_NAME = "org.sqlite.JDBC";
+    private static final String URL = "jdbc:sqlite:C:\\Users\\Luciano\\Desktop\\BancoSQLPOO\\disciplina.db";
     
     private String nome;
     private String ementa;
     private int ciclo;
     private Double nota;
-    private static List<Disciplina> lista = new ArrayList();
 
-    public Disciplina() {
+    public Disciplina(String nome) {
+        this.nome = nome;
+        
     }
 
     public Disciplina(String nome, String ementa, int ciclo) {
@@ -62,17 +69,66 @@ public class Disciplina {
         this.nota = nota;
     }
     
-    public static List<Disciplina> getList() {
+    public static ArrayList<String> select() throws Exception{
+        ArrayList<String> list = new ArrayList<>();
+        Class.forName(CLASS_NAME);
+        Connection con = DriverManager.getConnection(URL);
+        Statement stmt = con.createStatement();
+        DbListener.createTable();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM disciplina");
+        while(rs.next()){
+            list.add(rs.getString("name"));
+        }
+        rs.close();
+        stmt.close();
+        con.close();
+        return list;
+    }
+    public static void insert(String name) throws Exception{
+        Class.forName(CLASS_NAME);
+        Connection con = DriverManager.getConnection(URL);
+        Statement stmt = con.createStatement();
+        stmt.execute("insert into disciplina values('"+name+"')");
+        stmt.close();
+        con.close();
+    }
+    public static void delete(String name) throws Exception{
+        Class.forName(CLASS_NAME);
+        Connection con = DriverManager.getConnection(URL);
+        Statement stmt = con.createStatement();
+        stmt.execute("delete from disciplina where name = '"+name+"'");
+        stmt.close();
+        con.close();
+    }
+    
+    public static ArrayList<Disciplina> getList() throws Exception{
+        ArrayList<Disciplina> list = new ArrayList<>();
+        Statement stmt = null; ResultSet rs = null;
+        Exception methodException = null;
         
-        lista.add(new Disciplina ("Engenharia de Software III","Conceitos, evolução e importância de arquitetura de software. Padrões de Arquitetura. Padrões de Distribuição. Camadas no desenvolvimento de software. Tipos de Arquitetura de Software. Visões na arquitetura de software. Modelo de Análise e Projetos. Formas de representação. O processo de desenvolvimento. Mapeamento para implementação. Integração do sistema. Testes: planejamento e tipos. Manutenção. Documentação.", 4));
-        lista.add(new Disciplina ("Programação Orientada a Objetos","Conceitos e evolução da tecnologia de orientação a objetos. Limitações e diferenças entre o paradigma da programação estruturada em relação à orientação a objetos. Conceito de objeto, classe, métodos, atributos, herança, polimorfismo, agregação, associação, dependência, encapsulamento, mensagem e suas respectivas notações na linguagem padrão de representação da orientação a objetos. Implementação de algoritmos orientado a objetos utilizando linguagens de programação. Aplicação e uso das estruturas fundamentais da orientação a objetos.", 4));
-        lista.add(new Disciplina ("Linguagem de Programação IV","Comandos de linguagens usadas na construção e estruturação de sites para a Web, com páginas dinâmi­cas e interativas. Definição de layouts e formatação em geral. Adição de algorítmos usando laços, matrizes, datas, funções e condições. Introdução a Orientação a Objetos utilizando objetos, métodos e propriedades. Integração com Banco de Dados. Exercícios práticos com projeto de criação de sites.", 4));
-        lista.add(new Disciplina ("Programação para Dispositivos Móveis","Ambientes de programação para dispositivos móveis. Emuladores. Interface gráfica, serviços baseados em localização, armazenamento de dados persistentes, serviços de telefonia e comunicação entre processos. Desenvolvimento de aplicações com J2ME.", 5));
-        lista.add(new Disciplina ("Segurança da Informação","Requisitos de segurança de aplicações, de base de dados e de comunicações. Segurança de dispositivos móveis. Políticas de segurança. Criptografia. Firewalls. Vulnerabilidades e principais tecnologias de segurança.", 5));
-        lista.add(new Disciplina ("Sistemas Operacionais II","Apresentação de um sistema operacional específico utilizado em ambiente corporativo. Requisitos de hardware para instalação do sistema. Processo de instalação, personalização, operação, administração e segurança sobre o sistema operacional focado.  Elaboração de projetos de seleção e implantação de um sistema operacional.", 4));
-        lista.add(new Disciplina ("Metodologia da Pesquisa Científico","O Papel da ciência e da tecnologia. Tipos de conhecimento. Método e técnica. O processo de leitura e de análise textual. Citações e bibliografias. Trabalhos acadêmicos: tipos, características e composição estrutural. O projeto de pesquisa experimental e não-experimental. Pesquisa qualitativa e quantitativa. Apresentação gráfica. Normas da ABNT.", 4));
+        try {
+            Class.forName(CLASS_NAME);
+            Connection con = DriverManager.getConnection(URL);
+            stmt = con.createStatement();
+            
+            rs = stmt.executeQuery("SELECT * FROM disciplina");
         
-        return lista;
+            while(rs.next()) {
+            list.add(new Disciplina(rs.getString("nome")));
+            
+            }
+        }catch(Exception ex) {
+               
+               methodException = ex;
+        }finally{
+            try{rs.close();}catch(Exception ex2) {}
+            try{stmt.close();}catch(Exception ex2) {}
+        }
+        
+        
+        if (methodException !=null) throw methodException;
+        
+        return list;
     }
     
     
--- P1ProgOrienteda/src/java/web/DbListener.java
+++ P1ProgOrienteda/src/java/web/DbListener.java
@@ -4,11 +4,52 @@
  * and open the template in the editor.
  */
 package web;
+import java.sql.*;
+import br.fatec.disciplina.poo.Disciplina;
 
 /**
  *
  * @author Luciano
  */
 public class DbListener {
+    private static final String CLASS_NAME = "org.sqlite.JDBC";
+    private static final String URL = "jdbc:sqlite:C:\\Users\\Luciano\\Desktop\\BancoSQLPOO\\disciplina.db";
+    
+    public static void createTable() throws Exception {
+        Class.forName(CLASS_NAME);
+        Connection con = DriverManager.getConnection(URL);
+        Statement stmt = con.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS disciplina (nome  VARCHAR(100) UNIQUE NOT NULL)");
+        
+        stmt.close();
+        con.close();
+    }
+    
+    public static void discip() throws Exception {
+        Class.forName(CLASS_NAME);
+        Connection con = DriverManager.getConnection(URL);
+        Statement stmt = con.createStatement();
+        if (Disciplina.getList().isEmpty()) {
+            stmt.executeUpdate("INSERT INTO disciplina (nome)"
+                        + "VALUES ('Engenharia de Software III')");
+            stmt.executeUpdate("INSERT INTO disciplina (nome)"
+                        + "VALUES ('Programação Orientada a Objetos')");
+            stmt.executeUpdate("INSERT INTO disciplina (nome)"
+                        + "VALUES ('Linguagem de Programação IV - INTERNET')");
+            stmt.executeUpdate("INSERT INTO disciplina (nome)"
+                        + "VALUES ('Programação para Dispositivos Móveis')");
+            stmt.executeUpdate("INSERT INTO disciplina (nome)"
+                        + "VALUES ('Segurança da Informação')");
+            stmt.executeUpdate("INSERT INTO disciplina (nome)"
+                        + "VALUES ('Sistemas Operacionais II')");
+            stmt.executeUpdate("INSERT INTO disciplina (nome)"
+                        + "VALUES ('Metodologia da Pesquisa Científico-Tecnológica')");
+        }
+        
+        stmt.close();
+        con.close();
+    }
+    
+   
     
 }
